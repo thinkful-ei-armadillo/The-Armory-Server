3bb001d962753e74d212774bf056542f9a03cac5
Juicebox Jones <jderhalli@gmail.com>
2019-04-24 14:13:50 -0700

    patch working

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const app = express();
+const userRouter = require('./user/user-router');
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
