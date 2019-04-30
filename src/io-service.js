e9eafbb4cd191f6acc60d92b47f717f917c27f25
andrewbituin <fandrewbituin@gmail.com>
2019-04-30 11:25:01 -0700

    party chat using context to update and store chat log with back end assigning uuid and emitting message

--- src/io-service.js
+++ src/io-service.js
@@ -1,58 +1,73 @@
 let io;
 let app;
-const { requireSocketAuth } = require('./middleware/jwt-auth');
-const SpotService = require('./spots/spot-service');
-const PartyService = require('./party/party-service');
+const { requireSocketAuth } = require("./middleware/jwt-auth");
+const SpotService = require("./spots/spot-service");
+const PartyService = require("./party/party-service");
+const uuid = require("uuid");
 
 const ioService = {
   setUpIo(ioInstance) {
     io = ioInstance;
-    io.on('connection', function(socket) {
-      console.log('connected!');
-    
-      socket.on('leave party', async function(msg){
-        const db = app.get('db');
-        const {party_id, user_auth, room_id, game_id} = msg;
+    io.on("connection", function(socket) {
+      console.log("connected!");
+
+      socket.on("leave party", async function(msg) {
+        const db = app.get("db");
+        const { party_id, user_auth, room_id, game_id } = msg;
         try {
           const user_id = await requireSocketAuth(db, user_auth);
-          console.log('after await user auth', user_id)
+          console.log("after await user auth", user_id);
 
           const spot = await SpotService.findUserSpot(db, user_id, party_id);
-          console.log('spot_id', spot.id);
+          console.log("spot_id", spot.id);
 
-          await SpotService.updateSpot(db, spot.id, {filled: null});
+          await SpotService.updateSpot(db, spot.id, { filled: null });
           const { owner_id } = await PartyService.getOwnerId(db, party_id);
-          console.log('owner_id', owner_id);
+          console.log("owner_id", owner_id);
 
-          if(owner_id === user_id){
-            const newOwners = await SpotService.getNewOwnerId(db, owner_id, party_id);
-            console.log('newOwners', newOwners);
+          if (owner_id === user_id) {
+            const newOwners = await SpotService.getNewOwnerId(
+              db,
+              owner_id,
+              party_id
+            );
+            console.log("newOwners", newOwners);
 
-            if(newOwners.length < 1){
+            if (newOwners.length < 1) {
               await PartyService.deleteParty(db, party_id);
-              io.sockets.in(`/games/${game_id}`).emit('update parties', party_id);
+              io.sockets
+                .in(`/games/${game_id}`)
+                .emit("update parties", party_id);
             }
-            if(newOwners[0].filled){
-              await PartyService.updateParty(db, party_id, {owner_id:newOwners[0].filled});
+            if (newOwners[0].filled) {
+              await PartyService.updateParty(db, party_id, {
+                owner_id: newOwners[0].filled
+              });
             }
           }
-          const party = await PartyService.serializeParty(db, await PartyService.getPartyById(db, party_id))
-          console.log('party', party);
+          const party = await PartyService.serializeParty(
+            db,
+            await PartyService.getPartyById(db, party_id)
+          );
+          console.log("party", party);
 
-          io.sockets.in(room_id).emit('update party');
-          io.sockets.in(`/games/${game_id}`).emit('update parties');
-          console.log('game_id', game_id);
-    
-    
-        } catch(err) {
+          io.sockets.in(room_id).emit("update party");
+          io.sockets.in(`/games/${game_id}`).emit("update parties");
+          console.log("game_id", game_id);
+        } catch (err) {
           console.error(err);
           if (err.message === "Unauthorized request") {
-            io.to(`${socket.id}`).emit('post party error', 'Unauthorized request');
+            io.to(`${socket.id}`).emit(
+              "post party error",
+              "Unauthorized request"
+            );
             return;
           }
-          io.to(`${socket.id}`).emit('post party error', 'Something went wrong, check the party information');
+          io.to(`${socket.id}`).emit(
+            "post party error",
+            "Something went wrong, check the party information"
+          );
         }
-    
       });
       // socket.on('post party', async function(msg){
       //   let partyId;
@@ -76,7 +91,7 @@ const ioService = {
       //     partyId = await PartyService.insertParty(db, msg.party);
       //     //updates all spots and the roles associated with said spot
       //     await Promise.all([
-      //       await SpotService.insertSpot(db, partyId, [], userId), 
+      //       await SpotService.insertSpot(db, partyId, [], userId),
       //       await ReqService.insertReq(db, partyId, msg.requirement),
       //       ...msg.spots.map(async(spot) => {
       //         return await SpotService.insertSpot(db, partyId, spot.roles, spot.filled)
@@ -102,34 +117,31 @@ const ioService = {
       //   }
       // });
 
-      socket.on('join room', function(room_id) {
+      socket.on("join room", function(room_id) {
         socket.join(room_id);
       });
 
-      socket.on('chat message', function(messageData){
-        console.log(messageData);
-        this.emitRoomEvent('update chat', messageData.room_id, messageData.message);
-      })
-    
-      socket.on('leave game', function() {
+      socket.on("chat message", function(messageData) {
+        const roomId = messageData.room_id;
+        messageData.message_id = uuid();
+        io.sockets.in(roomId).emit("update chat", messageData);
+      });
+
+      socket.on("leave game", function() {
         console.log(socket.id);
       });
-    
-      socket.on('disconnect', function() {
-        console.log('disconnected');
+
+      socket.on("disconnect", function() {
+        console.log("disconnected");
       });
-    
-    
     });
   },
   emitRoomEvent(event_name, room_id, data) {
     io.sockets.in(room_id).emit(event_name, data);
   },
-  setApp(appInstance){
-    app = appInstance
+  setApp(appInstance) {
+    app = appInstance;
   }
 };
 
 module.exports = ioService;
-
-
